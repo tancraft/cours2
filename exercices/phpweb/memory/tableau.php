7915fac4738dd051ede15afc4f6d5c7b1b818a7f
59011-95-12 <59011-95-12@exchange.ad.afpanet>
2020-11-12 12:25:03 +0100

    12/11/2020 12:25:03,35

--- exercices/phpweb/memory/tableau.php
+++ exercices/phpweb/memory/tableau.php
@@ -1,11 +1,21 @@
 <?php
 
+for ($i=1;$i<9;$i++)  
+{
+    $tab[]=$i;
+    $tab[]=$i;
+  
+} 
+
 for ($i = 1; $i < 5; $i++) {
    echo '<div class="espace"></div>';
     echo '<div class="ligne">';
     for ($j = 1; $j < 5; $j++) 
     {
-        $nb= rand(1,8);
+        $key = array_rand($tab);
+        $nb = $tab[$key];
+        //on enlève la valeur du tableau
+        array_splice($tab,$key,1);
         echo '<div class="espaceHor">
         </div><div class="case">
 <img class="recto" src="images/plage.jpg" alt="">
