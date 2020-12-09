55a28803b1968ea35b765e4d4c22d28a8bfa49a0
59011-95-12 <59011-95-12@exchange.ad.afpanet>
2020-12-09 12:25:03 +0100

    09/12/2020 12:25:02,04

---
+++ virtualHost/1.test/script.js
@@ -0,0 +1,23 @@
+var myVar = 123; maFonction();
+
+ function maFonction() 
+ { 
+     console.log("myVar fonction : "+myVar);
+ } 
+ if (myVar > 1) 
+ { 
+     console.log("myVar condition : "+myVar);
+ } 
+ console.log("myVar fin : "+myVar);
+
+
+ var compteur = 2; function maFonction() {
+      var myVar = 456; console.log("myVar : "+myVar); 
+    } 
+    if (compteur > 1) 
+    { let myVar2 = "Wazaa !"; console.log("myVar2 : "+myVar2);
+}
+ /* Ici, myVar n'est pas disponible * car déclarée dans la fonction * sa portée ne concerne que le code de la fonction */ 
+ console.log("myVar : "+myVar);
+/* Ici, myVar2 n’est pas disponible * car déclarée dans le bloc de condition * avec une portée uniquement pour ce bloc */ 
+console.log("myVar2 : "+myVar2);
