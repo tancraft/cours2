4d10afcdc088fd225d49b8d1d53533a57df543c5
59011-95-12 <59011-95-12@exchange.ad.afpanet>
2020-12-09 17:26:47 +0100

    09/12/2020 17:26:46,98

--- exercices/JS/conditions/EX4/script.js
+++ exercices/JS/conditions/EX4/script.js
@@ -6,17 +6,17 @@ var _tot = _pu*_qtcom;
 
 if (_tot>500)
 {
-    var _remtot = 0
+    var _portot = 0
 }
 else
 {
-    var _rem = _tot*2/100;
-    if(_rem >6)
+    var _por = _tot*2/100;
+    if(_por >6)
     {
-       var _remtot = _rem;
+       var _portot = _rem;
     }
     else
     {
-        var _remtot = 6;
+        var _portot = 6;
     }
 }
