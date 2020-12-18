79320254d87c12eb4118899e1781cb119a4c645f
tancraft <tancraftwork@gmail.com>
2020-12-18 11:47:33 +0100

    18/12/2020 11:47:33,43

--- exercices/JS/DOM/deplacements 2/script.js
+++ exercices/JS/DOM/deplacements 2/script.js
@@ -1,7 +1,7 @@
 
-function mouvement(dirleft,dirTop)// dans les parametres ne pas inverser left et top
+function mouvement(dirleft, dirTop)// dans les parametres ne pas inverser left et top
 {
-    // permet de deplacer le carre selon 2 directions left et top
+    // permet de deplacer le joueur selon 2 directions left et top
     //on recupere son style (tout le CSS)
     var styleJoueur = window.getComputedStyle(joueur);
     //on recupere les positions left et top actuelles
@@ -13,19 +13,14 @@ function mouvement(dirleft,dirTop)// dans les parametres ne pas inverser left et
     joueur.style.left = parseInt(leftActuel) + dirleft + 'px';
 }
 
-function deplace()
-{
-    
-}
-
 var joueur = document.getElementById('joueur');
 var left = document.getElementById("left");
 var right = document.getElementById("right");
 var up = document.getElementById("up");
 var down = document.getElementById("down");
 
-left.addEventListener("mousedown", function() {
- mouvement(-5, 0); 
+left.addEventListener("mousedown", function () {
+    mouvement(-5, 0);
 });
 
 right.addEventListener("click", function () {
@@ -38,4 +33,54 @@ down.addEventListener("click", function () {
     mouvement(0, 5);
 });
 
-//plusieur test fait sur cette page pour activer remanence en click sur bouton interface avec set interval mais infructueux
+//gestion au clavier
+window.addEventListener("keydown", function (event) {
+    switch (event.key) {
+
+        case "ArrowLeft":
+            {
+                mouvement(-5, 0);
+                break;
+            }
+        case "ArrowRight":
+            {
+                mouvement(5, 0);
+                break;
+            }
+        case "ArrowUp":
+            {
+                mouvement(0, -5);
+                break;
+            }
+        case "ArrowDown":
+            {
+                mouvement(0, 5);
+                break;
+            }
+    }
+
+
+});
+
+//gestion a la souris
+var flag = false;
+var ecartY,ecartX;
+
+joueur.addEventListener("mousedown", (e) => {
+    flag = true;
+    ecartY = parseInt(window.getComputedStyle(joueur).top) - parseInt(e.clientY);
+    ecartX = parseInt(window.getComputedStyle(joueur).left) - parseInt(e.clientX);
+    
+});
+
+document.addEventListener("mousemove", (e) => {
+    if (flag == true)
+    {
+        joueur.style.top = parseInt(e.clientY) + ecartY + "px";
+        joueur.style.left = parseInt(e.clientX) + ecartX + "px";
+    }
+});
+
+joueur.addEventListener("mouseup", function() {
+    flag = false;
+});
