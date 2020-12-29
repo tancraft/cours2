9227e8788acd2fb0fa4dc390603f435dc4f1ea06
tancraft <tancraftwork@gmail.com>
2020-12-29 18:21:22 +0100

    29/12/2020 18:21:21,93

--- virtualHost/cookieDl/script.js
+++ virtualHost/cookieDl/script.js
@@ -1,3 +1,4 @@
+//global
 function createCookie(name, value, days) {
     // permet de créer un cookie
     if (days) {
@@ -5,8 +6,7 @@ function createCookie(name, value, days) {
         var date = new Date();
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         var expires = "expires=" + date.toGMTString();
-    }
-    else var expires = "";
+    } else var expires = "";
     //le cookie doit contenir  clé=valeur;expires=temps;path=nomDomaine
     document.cookie = name + "=" + value + "; " + expires + "; path=/";
 }
@@ -16,7 +16,6 @@ function readCookie(name) {
     var listeCookies = document.cookie.split(';');//on cree le tableau de la liste des cookies
 	for(var i=0;i < listeCookies.length;i++) {// on boucle sur la longueur de ce tableau
         var unCookie = listeCookies[i];//on cree un tableau avec le texte de chaque cookie different
-        console.log(unCookie);
     // on boucle si le premier caractere du string est un espace; sinon on soustrait cet espace en creant une nouvelle chaine commence a l index 1
         while (unCookie.charAt(0)==' ') unCookie = unCookie.substring(1,unCookie.length);
         // on verifie maintenant que si l indice 0 coresspond au nom cu cookie on refait un tour dans le for sinon on decoupe la longuer du nom du cookie et ensuite on renvoie enfin la chaine qui correspond a l information
@@ -31,6 +30,50 @@ function eraseCookie(name) {
     createCookie(name, "", -1);
 }
 
+//changement de couleur
+function changeMode(monMode)
+{
+    if (monMode.getAttribute('value') == 2) {
+        monMode.setAttribute('value', 1);
+    } else {
+        monMode.setAttribute('value', 2);
+    }
+    createCookie("couleur", monMode.value, 5);
+    return monMode.value;
+}
+
+function changeCouleur(maValeur) {
+    if (maValeur == 2) {
+        nouvClass = "dark";
+        monMode.value = 2;
+        monMode.innerHTML = nouvClass;
+    } else {
+        nouvClass = "light";
+        monMode.value = 1;
+        monMode.innerHTML = nouvClass;
+    }
+    mesParagraphes = document.getElementsByClassName("paragraphe");
+    for (let i = 0; i < mesParagraphes.length; i++) {
+        mesParagraphes[i].setAttribute("class", "paragraphe " + nouvClass);
+    }
+    document.body.setAttribute("class", nouvClass);
+    createCookie("couleur", monMode.value, 5);
+}
+
+//changement de couleur
+var monMode = document.getElementById('mode');
+monMode.addEventListener("click", function () {
+    changeMode(monMode);
+});
+
+monMode.addEventListener("click", function () {
+    changeCouleur(monMode.value);
+});
+
+maCouleur = readCookie("couleur");
+changeCouleur(maCouleur);
+
+//le compteur
 // du coup je reutilise index of que j aime bien et on verifie si le cookie est present sur le domaine
 if (document.cookie.indexOf('compteur') != -1)
 { 
@@ -43,10 +86,18 @@ if (document.cookie.indexOf('compteur') != -1)
 } 
 else 
 {
-//alert('cookie n est pas la ');
 var compteur =1;
 createCookie("compteur", compteur, 1);
 var compte = readCookie("compteur");
 document.getElementById("compteVue").innerHTML = compte;
 
 }
+
+//bouton reset de cookie compteur
+var reset=document.getElementById('reset');
+reset.addEventListener("click",function(){
+    var compte = readCookie("compteur");
+    compte = 0;
+    document.getElementById("compteVue").innerHTML = compte;
+    createCookie("compteur", compte, 1);
+});
