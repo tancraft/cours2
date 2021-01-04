be50268ea03db5fb67ba1d3f73ab99b59343326c
tancraft <tancraftwork@gmail.com>
2021-01-04 11:41:28 +0100

    04/01/2021 11:41:27,81

---
+++ exercices/JS/DOM/formulaire/script.js
@@ -0,0 +1,10 @@
+var valide = document.querySelector('input');
+var form = document.querySelector()
+
+document.forms[0].addEventListener('submit', function(){
+    if (valide.value == "")
+    {
+        alert('toto');
+    }
+}
+);
