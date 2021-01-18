4ae5aac941c012d6f2407e82f67492d1606078d1
tancraft <tancraftwork@gmail.com>
2021-01-18 14:35:42 +0100

    18/01/2021 14:35:41,86

--- virtualHost/convention/PHP/VIEW/sessionList.php
+++ virtualHost/convention/PHP/VIEW/sessionList.php
@@ -1,13 +1,14 @@
 <?php
 
-                    echo '<section class = "colonne"><h2>Liste des sessions</h2>
-                    <div><button class="bouton"><i class="fas fa-plus-circle"></i><a href="Index.php?codePage=details&categorie=repres&mode=ajout"> Ajouter</a></div><div></div>';
+                    echo '<section class = "colonne"><div class = "case centre noborder"><h2>Liste des sessions</h2></div>
+                    <div><a href="Index.php?codePage=details&categorie=repres&mode=ajout"><button class="bouton"><i class="fas fa-plus-circle"></i> Ajouter</button></a></div><div></div>
+                    <div class="info"><div class="case double">Libelle formation</div><div class="case">numéro d\'offre</div><div class="double"></div></div>';
                     $tableau = SessionformationManager::getList();
                     foreach ($tableau as $uneSession) {
                         $formation = FormationsManager::findById($uneSession->getIdFormation());
-                        echo '<div class="list"><div class="case"> '.substr($formation->getLibelleFormation(),0,-12).'</div><div class="case"> '.$uneSession->getNumOffreFormation().'</div><div class="case"> '.$uneSession->getDateRapportSuivi().'</div>'.
-                        '<div><button class="bouton"><i class="fas fa-edit"></i><a href="Index.php?codePage=details&categorie=repres&mode=modif&id='.$uneSession->getIdSessionFormation().'"> Modifier</a></button>
-                        <button class="bouton"><i class="fas fa-info-circle"></i><a href="Index.php?codePage=details&categorie=repres&mode=detail&id='.$uneSession->getIdSessionFormation().'"> Détail</a></button>
-                        <button class="bouton"><i class="fas fa-trash-alt"></i><a href="Index.php?codePage=details&categorie=repres&mode=delete&id='.$uneSession->getIdSessionFormation().'"> Supprimer</a></button></div></div>';
+                        echo '<div class="info"><div class="case double centre"> '.$formation->getLibelleFormation().'</div><div class="case centre"> '.$uneSession->getNumOffreFormation().'</div>'.
+                        '<div class="double centre"><a href="Index.php?codePage=details&categorie=repres&mode=modif&id='.$uneSession->getIdSessionFormation().'"><button class="bouton"><i class="fas fa-edit"></i> Modifier</button></a><div class="mini"></div>
+                        <a href="Index.php?codePage=details&categorie=repres&mode=detail&id='.$uneSession->getIdSessionFormation().'"><button class="bouton"><i class="fas fa-info-circle"></i> Détail</button></a><div class="mini"></div>
+                        <a href="Index.php?codePage=details&categorie=repres&mode=delete&id='.$uneSession->getIdSessionFormation().'"><button class="bouton"><i class="fas fa-trash-alt"></i> Supprimer</button></a></div></div>';
                     }
                     echo '</section>';
