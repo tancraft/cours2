71e711bf59fb75dbe3117e2132c1d39fd095775c
tancraft <tancraftwork@gmail.com>
2021-01-18 10:46:15 +0100

    18/01/2021 10:46:14,56

--- virtualHost/convention/PHP/VIEW/sessionList.php
+++ virtualHost/convention/PHP/VIEW/sessionList.php
@@ -5,7 +5,7 @@
                     $tableau = SessionformationManager::getList();
                     foreach ($tableau as $uneSession) {
                         $formation = FormationsManager::findById($uneSession->getIdFormation());
-                        echo '<div class="list"><div class="case"> '.$uneSession->getObjectifPAE().'</div><div class="case"> '.$uneSession->getDateRapportSuivi().'</div><div> '.substr($formation->getLibelleFormation(),0,-12).'</div>'.
+                        echo '<div class="list"><div class="case"> '.substr($formation->getLibelleFormation(),0,-12).'</div><div class="case"> '.$uneSession->getNumOffreFormation().'</div><div class="case"> '.$uneSession->getDateRapportSuivi().'</div>'.
                         '<div><button class="bouton"><i class="fas fa-edit"></i><a href="Index.php?codePage=details&categorie=repres&mode=modif&id='.$uneSession->getIdSessionFormation().'"> Modifier</a></button>
                         <button class="bouton"><i class="fas fa-info-circle"></i><a href="Index.php?codePage=details&categorie=repres&mode=detail&id='.$uneSession->getIdSessionFormation().'"> Détail</a></button>
                         <button class="bouton"><i class="fas fa-trash-alt"></i><a href="Index.php?codePage=details&categorie=repres&mode=delete&id='.$uneSession->getIdSessionFormation().'"> Supprimer</a></button></div></div>';
