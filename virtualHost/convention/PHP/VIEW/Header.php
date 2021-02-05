017ddac358cc27749c78baabbce89ec935bdfe11
59011-95-12 <59011-95-12@exchange.ad.afpanet>
2021-02-05 11:04:24 +0100

    05/02/2021 11:04:22,87

--- virtualHost/convention/PHP/VIEW/Header.php
+++ virtualHost/convention/PHP/VIEW/Header.php
@@ -9,13 +9,17 @@
         <h2><?php echo $titre ?></h2>
     </div>
 
+
+
     <div class="connection colonne centre">
         <div></div>
         <?php
+        //ajout du nom prenom quand utilisateur connecter
             if (isset($_SESSION['utilisateur'])) {
+                echo '<div class="texteClair" >Bonjour ' . $_SESSION['utilisateur']->getPrenomUtilisateur().' '.$_SESSION['utilisateur']->getNomUtilisateur().'</div>';
                 echo '<a href="index.php?page=ActionConnexion&mode=logout">
 
-                    <button class="bouton centre">Deconnection</button>
+                    <button class="bouton centre"><i class="fas fa-sign-out-alt"></i> Deconnection</button>
 
                     </a>';
             }
